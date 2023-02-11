bae4acaac860e476eacce47effabf5e815250be8
Bhushan-shrirame <bhushanshrirame24@gmail.com>
2023-02-11 20:07:58 +0530

    fix bugs

--- server/controllers/Answers.js
+++ server/controllers/Answers.js
@@ -30,24 +30,22 @@ const updateNoOfQuestioins = async (_id, noOfAnswers) => {
 };
 
 export const deleteAnswer = async (req, res) => {
-    const {id:_id} = req.params;
-    const {answerId , noOfAnswers} = req.body;
+  const { id: id } = req.params;
+  const { answerId, noOfAnswers } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).send("question unavailable...");
-    }
-    if (!mongoose.Types.ObjectId.isValid(answerId)) {
-      return res.status(404).send("Answer unavailable...");
-    }
-    updateNoOfQuestioins(_id,noOfAnswers)
-    try {
-       await Questions.updateOne(
-        { _id},
-        {$pull: {'answer':{_id: answerId } } }
-       )
-       res.status(200).json({message: "Sucessfully deleted..."})
-    } catch (error) {
-      res.status(405).json(error)
-      
-    }
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("question unavailable...");
+  }
+  if (!mongoose.Types.ObjectId.isValid(answerId)) {
+    return res.status(404).send("Answer unavailable...");
+  }
+
+  updateNoOfQuestioins(id, noOfAnswers);
+  try {
+    await Questions.updateOne({ id }, { $pull: { answer: { _id: answerId } } });
+    res.status(200).json({ message: "Sucessfully deleted..." });
+  } catch (error) {
+    console.log(error);
+    res.status(405).json(error);
+  }
+};
--- server/routes/Answers.js
+++ server/routes/Answers.js
@@ -4,6 +4,6 @@ import {postAnswer , deleteAnswer} from '../controllers/Answers.js'
 const router = express.Router();
 
 router.patch('/post/:id',postAnswer)
-router.patch('/ delete/:id',deleteAnswer)
+router.patch('/delete/:id',deleteAnswer)
 
 export default router
