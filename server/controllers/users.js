16bba5973ad490b20bebff6260ce22c7fb2e2b8e
Bhushan-shrirame <bhushanshrirame24@gmail.com>
2023-02-14 15:02:20 +0530

    user profile backend and frontend integration

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,10 +6,26 @@ export const getAllUsers = async (req , res) =>{
       const allUsers = await User.find();
       const allUserDetails = []
       allUsers.forEach(users => {
-        allUserDetails.push({id: users.id, name: users.name, about: users.about, tags : users.tags, joinedOn: users.joinedOn})
+        allUserDetails.push({id: users._id, name: users.name, about: users.about, tags : users.tags, joinedOn: users.joinedOn})
       })
       res.status(200).json(allUserDetails)
    } catch (error) {
         res.status(404).json({message: error.message})
+   }
+}
+
+export const updateProfile = async (req, res) => {
+   const { id: _id } = req.params;
+   const { name, about, tags } = req.body;
+
+   if(!mongoose.Types.ObjectId.isValid(_id)){
+       return res.status(404).send('question unavailable...');
+   }
+
+   try {
+       const updatedProfile = await users.findByIdAndUpdate( _id, { $set: { 'name': name, 'about': about, 'tags': tags }}, { new: true } )
+       res.status(200).json(updatedProfile)
+   } catch (error) {
+       res.status(405).json({ message: error.message })
    }
 }
