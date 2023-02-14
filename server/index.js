a9fcdbbb9d1ab240b50f62ae29643ab17e98603f
Bhushan-shrirame <bhushanshrirame24@gmail.com>
2023-02-14 15:20:45 +0530

    final

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
 import cors from 'cors'
+import dotenv from 'dotenv'
 import userRoutes from "./routes/users.js";
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app = express();
+dotenv.config();
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
@@ -20,8 +22,7 @@ app.use('/answer',answerRoutes)
 
 const PORT = process.env.PORT || 5000
 
-// const CONNECTION_URL = "mongodb+srv://admin:admin@cluster0.idk4jhr.mongodb.net/?retryWrites=true&w=majority"
-const CONNECTION_URL = "mongodb+srv://admin:admin@stack-overflow-clone.yumimj1.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = process.env.CONNECTION_URL
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
    .then(() => app.listen(PORT,() => {console.log(`server running on port ${PORT}`)}))
